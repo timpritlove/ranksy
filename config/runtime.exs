ff6c570559bc6645b343f090ee1d0c11218b071d
Tim Pritlove <tim@pritlove.org>
2025-07-02 00:33:35 +0200

    improve fly deployment

--- config/runtime.exs
+++ config/runtime.exs
@@ -61,11 +61,8 @@ if config_env() == :prod do
   config :ranksy, RanksyWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
     http: [
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      # See the documentation on https://hexdocs.pm/bandit/Bandit.html#t:options/0
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
+      # Enable IPv4 and bind on all interfaces for Fly.io compatibility.
+      ip: {0, 0, 0, 0},
       port: port
     ],
     secret_key_base: secret_key_base
